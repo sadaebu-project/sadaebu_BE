328f2d9ed90abc71f07c92d33944b268a51e907f
nadomolayohehe <siyun778@naver.com>
2024-02-16 22:52:05 +0900

    :alien: First Commit
    first commit

---
+++ final_sadaebu/src/main/java/com/example/final_sadaebu/FinalSadaebuApplication.java
@@ -0,0 +1,13 @@
+package com.example.final_sadaebu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalSadaebuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalSadaebuApplication.class, args);
+	}
+
+}
